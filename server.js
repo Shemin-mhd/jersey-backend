8fb35423977102d74426659ac6dcce4f12ee4185
unknown <sheminmuhammed594@gmail.com>
2026-06-20 19:45:52 +0530

    latest

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(
       "http://localhost:5174",
       "http://localhost:3000",
       "http://127.0.0.1:5173",
-      "https://jersey-frontend-seven.vercel.app/", // Replace this
+      "https://jersey-frontend-seven.vercel.app", // Replace this
     ],
     credentials: true,
   })
